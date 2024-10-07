ff291693aeab4d47ba72f9509c75617c917944c8
yashninja21 <149756690+yashninja21@users.noreply.github.com>
2024-10-07 16:03:20 +0530

    Complaint management

---
+++ users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+           Change Password only if its necessary!
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
